6a40ef080d1f65a5c0a1c4b8d21b6c27c1ef347e
Nicolai <cwX3MfXK9YrCCc8eZk@gmail.com>
2024-04-23 04:25:25 +0200

    Prepare deployment

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import Saga from './src/Saga.js';
 import SagaBuilder from './src/SagaBuilder.js';
 import SagaHandler from './src/SagaHandler.js';
 import SagaStep from './src/SagaStep.js';
+import BrokerService from './src/BrokerService.js';
 
 export default {
     Saga,
     SagaBuilder,
     SagaHandler,
-    SagaStep
+    SagaStep,
+    BrokerService
 };
